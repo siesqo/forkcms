284ee3a3bc93bc9a5c04a9ac92cb11baeb6f9778
Jeroen Desloovere <info@jeroendesloovere.be>
2018-03-22 08:52:07 +0100

    Add Siesqo theme FormBuilder javascript

--- src/Frontend/Themes/Siesqo/src/Js/Index.js
+++ src/Frontend/Themes/Siesqo/src/Js/Index.js
@@ -2,7 +2,7 @@
 import 'bootstrap/dist/js/bootstrap'
 
 /* Utilities imports */
-import {BackToTop} from './Utilities/BackToTop'
+import { BackToTop } from './Utilities/BackToTop'
 import SweetScroll from 'sweet-scroll'
 import { Resize } from './Utilities/Resize'
 /* import {Fancybox} from './Utilities/Fancybox' */
@@ -10,11 +10,13 @@ import { Resize } from './Utilities/Resize'
 /* Theme imports */
 /* eg. import tooltip from './Theme/Tooltip' */
 import { Pagination } from './Theme/Pagination'
+import { FormBuilder } from './Theme/FormBuilder'
 
 /* Renders */
 window.sweetscroll = new SweetScroll()
 window.resizeFunction = new Resize()
 window.pagination = new Pagination()
+window.formBuilder = new FormBuilder()
 
 window.resizeFunction.resize()
 window.pagination.events()
