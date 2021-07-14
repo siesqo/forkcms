38c66345c9bb5cda8148abd4cb63c305afd56d65
Alessandro Craeye <alessandro@siesqo.be>
2021-07-14 23:11:45 +0200

    Remove unused column generator for FormBuilder forms

--- src/Frontend/Themes/Siesqo/src/Js/Theme/FormBuilder.js
+++ src/Frontend/Themes/Siesqo/src/Js/Theme/FormBuilder.js
@@ -1,39 +1,9 @@
 export class FormBuilder {
-  constructor () {
+  constructor() {
     var $formHolder = $('.widget-formbuilder-form')
 
-    if ($formHolder.length === 0) {
-      return false
-    }
+    // Custom form theming
+    // ...
 
-    if ($(window).width() <= 960) {
-      return false
-    }
-
-    var $form = $formHolder.find('form')
-    var $formRows = $form.find('.row')
-
-    // we have more fields, so we split in two columns
-    if ($formRows.length > 5) {
-      var i = 0
-      var columnIndex = 0
-      var steps = 4
-      var $formRow = $('<div id="formRow" class="row"/>')
-      $form.append($formRow)
-
-      $formRows.each(function () {
-        var bingo = (i === 0 || i === steps)
-        var $paragraph = $(this)
-
-        if (bingo) {
-          columnIndex += 1
-          $formRow.append($('<div class="col-12 col-md-5" id="column-' + columnIndex + '" class="column"></div>'))
-        }
-
-        i += 1
-
-        $('#column-' + columnIndex).append($paragraph)
-      })
-    }
   }
-};
+}
