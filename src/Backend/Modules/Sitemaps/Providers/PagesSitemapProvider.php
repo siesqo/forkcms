ea451516d56fbd5a95af171f3c74906261595ef4
Alessandro Craeye <alessandro@siesqo.be>
2024-10-22 09:29:47 +0200

    Add extra failsafe to pages sitemap provider
    Preventing an error if the page info could not be fetched

--- src/Backend/Modules/Sitemaps/Providers/PagesSitemapProvider.php
+++ src/Backend/Modules/Sitemaps/Providers/PagesSitemapProvider.php
@@ -26,29 +26,31 @@ class PagesSitemapProvider extends SitemapProvider implements SitemapProviderInt
                 foreach ($items as $parentId => $children) {
                     foreach ($children as $pageId => $page) {
                         $pageInfo = Model::get($page['page_id']);
-                        $lastModifiedOn = date_timestamp_set(new \DateTime(), (int)$pageInfo['edited_on']);
-                        $priority = 5;
-                        $changeFrequency = ChangeFrequency::weekly();
+                        if ($pageInfo) {
+                            $lastModifiedOn = date_timestamp_set(new \DateTime(), (int)$pageInfo['edited_on']);
+                            $priority = 5;
+                            $changeFrequency = ChangeFrequency::weekly();
 
-                        // Home page
-                        if (in_array($pageId, [0, 1])) {
-                            $priority = 10;
-                            $changeFrequency = ChangeFrequency::always();
-                        }
+                            // Home page
+                            if (in_array($pageId, [0, 1])) {
+                                $priority = 10;
+                                $changeFrequency = ChangeFrequency::always();
+                            }
 
-                        // Exclude the following:
-                        // - 404 pages
-                        // - Module actions (these need to be added via the sitemap provider for the specific module)
-                        // - Footer pages (since these are marked as noindex and will cause errors in Search Console)
-                        // - Hidden pages (since these will return a 404 which causes errors in Search Console)
-                        // - Pages marked as nofollow (nofollow / noindex pages can't be picked up by Search Console)
-                        if ($pageId != 404 && $page['tree_type'] != 'direct_action' && $navigationType != 'footer' && $pageInfo['hidden'] != 1 && !$page['no_follow']) {
-                            $this->createItem(
-                                $page['full_url'],
-                                $lastModifiedOn,
-                                $changeFrequency,
-                                $priority
-                            );
+                            // Exclude the following:
+                            // - 404 pages
+                            // - Module actions (these need to be added via the sitemap provider for the specific module)
+                            // - Footer pages (since these are marked as noindex and will cause errors in Search Console)
+                            // - Hidden pages (since these will return a 404 which causes errors in Search Console)
+                            // - Pages marked as nofollow (nofollow / noindex pages can't be picked up by Search Console)
+                            if ($pageId != 404 && $page['tree_type'] != 'direct_action' && $navigationType != 'footer' && $pageInfo['hidden'] != 1 && !$page['no_follow']) {
+                                $this->createItem(
+                                    $page['full_url'],
+                                    $lastModifiedOn,
+                                    $changeFrequency,
+                                    $priority
+                                );
+                            }
                         }
                     }
                 }
